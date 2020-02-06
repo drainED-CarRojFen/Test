f150fd6c7a93b1ddad47db690c8dd61143e5e72a
name <mail>
2020-02-06 13:19:07 +0100

    New version

--- Hola Mundo/src/holamundo.java
+++ Hola Mundo/src/holamundo.java
@@ -4,8 +4,11 @@ public class holamundo {
 	public static void main(String [] args) {
 		
 		
-		System.out.println( " Hola mundo " );
-		
+		System.out.println( " Hola mundo ");
+		System.out.println( " Mundo ");
+		System.out.println( " Mundo ");
+		System.out.println( " eco " );
+	
 	}
 	
 	
